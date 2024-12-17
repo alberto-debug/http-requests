d8e2a8c62e0ca5b9ec848c00a226bd5d1e0b331f
Alberto <albertojunior848423803@gmail.com>
2024-12-18 01:46:12 +0300

    updated Controller

--- src/main/java/com/httprequests/Requests/Controller/StudentController.java
+++ src/main/java/com/httprequests/Requests/Controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.httprequests.Requests.Controller;
 
+import com.httprequests.Requests.Repository.StudentRepository;
 import com.httprequests.Requests.Service.StudentService;
 import com.httprequests.Requests.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,5 +17,51 @@ import java.util.Optional;
 @RequestMapping("/students")
 public class StudentController {
 
+    @Autowired
+    private StudentService studentService;
+
+    //register student
+    @PostMapping("/register")
+    public ResponseEntity<Student> registerStudent(@RequestBody Student student){
+
+        Student registeredStudent = studentService.registerStudent(student);
+        return ResponseEntity.ok(registeredStudent);
+    }
+
+    //Get all Students
+    @GetMapping("/")
+    public ResponseEntity<List<Student>> getAllStudents(){
+        List<Student> students = studentService.getAllStudents();
+        if (students.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentsById(@PathVariable  String id){
+        Optional<Student> student = studentService.getStuentById(id);
+        return student.map(s -> new ResponseEntity<>(s, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<Student> getStudentByEmail(@PathVariable String email){
+        Student student = studentService.getStudentByEmal(email);
+        if (student != null){
+            return new ResponseEntity<>(student, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+    //Delete Students by id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStudentById(@PathVariable String id){
+        boolean deleted = studentService.deleteStudentById(id);
+        return deleted ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 
 }
